a76062750875711df58b7fcf9c95d3b9a74c7baa
Manikandan Sekar <manikandansekare@gmail.com>
2026-06-09 07:40:29 +0530

    login test

---
+++ tests/login/login.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../../src/pages/LoginPage';
+
+test('Valid Login', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.navigate();
+
+  await loginPage.login(
+    'standard_user',
+    'secret_sauce'
+  );
+
+  await expect(page).toHaveURL(/inventory/);
+});
