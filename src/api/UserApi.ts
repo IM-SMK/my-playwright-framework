bcc586973e89951b3f93911db7fb7df9c74b8bd1
Manikandan Sekar <manikandansekare@gmail.com>
2026-06-14 23:17:56 +0530

    constructor exp

--- src/api/UserApi.ts
+++ src/api/UserApi.ts
@@ -1,6 +1,11 @@
 import { APIRequestContext } from "@playwright/test";
 
 export class UserApi {
+    //equivalent to
+    // private request: APIRequestContext;
+    // constructor(request: APIRequestContext) {
+    //     this.request = request;
+    // }
     constructor(private request: APIRequestContext) { }
     async getUsers() {
         return await this.request.get(
