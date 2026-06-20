dc538d20cd062e8759891d2b286f5efc4a3a3e25
Manikandan Sekar <manikandansekare@gmail.com>
2026-06-20 23:49:02 +0530

    added test match for config

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,11 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
 
+  testMatch: [
+    '**/*.spec.ts',
+    '**/*.e2e.ts',
+  ],
+
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
